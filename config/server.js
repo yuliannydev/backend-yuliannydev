80da2476d790f87061835fe1af3ac05d0c730333
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-25 23:09:53 -0400

    :tada: Begin a project.
    Project backend from official website

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '16509f811b386912eaf9306208a8e2b5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f6662816-a269-415e-a075-ee89c888065f'
+};
